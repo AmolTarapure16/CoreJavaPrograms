2f6c29ad4c0e6bbc90134ccb0c3ade9eb00c3759
AmolTarapure16 <amoltarapure16@gmail.com>
2025-07-04 22:57:21 +0530

    Interview_006_ReverseLastWordWithoutBuilt
    Interview_006_ReverseLastWordWithoutBuilt

--- src/com/interviewPrograms/Interview_006_ReverseLastWordWithoutBuilt.java
+++ src/com/interviewPrograms/Interview_006_ReverseLastWordWithoutBuilt.java
@@ -3,27 +3,48 @@ package com.interviewPrograms;
 public class Interview_006_ReverseLastWordWithoutBuilt
 {
      //Reverse String LastWord
-	 // Input -> Core Java Programming
-	 // Output-> Core Java gnimmargorP
+	 // Input -> Java programming language
+	 // Output-> Java programming egaugnal
 	
 	public static void main(String[] args) 
 	{
-		String input = "Core Java Programming";
-		String rev = "";
-		String splitArr[] = input.split(" ");
-		
-		String lastWord = splitArr[splitArr.length-1];
-		
-		for(int i =lastWord.length()-1; i>-1; i--)
-		{
-		rev = rev +lastWord.charAt(i);
-		}
-		
-		String middleWord= splitArr[splitArr.length-2];
-		String firstWord = splitArr[splitArr.length-3];
-		
-		System.out.println(firstWord + " " + middleWord +" " + rev );
+		String input = "Java programming language";
+        
+        // Step 1: Find the index of last space
+        int lastSpaceIndex = -1;
+        for (int i = input.length() - 1; i >= 0; i--) 
+        {
+            if (input.charAt(i) == ' ') 
+            {
+                lastSpaceIndex = i;
+                break;
+            }
+        }
 
-	}
+        // Step 2: Split prefix and last word
+        String prefix = "";
+        String lastWord = "";
 
+        if (lastSpaceIndex == -1) 
+        {
+            // Only one word in the input
+            lastWord = input;
+        } 
+        else 
+        {
+            prefix = input.substring(0, lastSpaceIndex + 1);
+            lastWord = input.substring(lastSpaceIndex + 1);
+        }
+
+        // Step 3: Manually reverse the last word
+        String reversedLastWord = "";
+        for (int i = lastWord.length()- 1; i >= 0; i--) 
+        {
+            reversedLastWord += lastWord.charAt(i);
+        }
+
+        // Step 4: Combine and print
+        String result = prefix + reversedLastWord;
+        System.out.println("Reversed Last Word String: " + result);
+    }
 }
