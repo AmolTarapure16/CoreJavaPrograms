5c32de296a9751ae066335044540f6caa1cbce0d
AmolTarapure16 <amoltarapure16@gmail.com>
2025-07-04 22:53:20 +0530

    First Word
    First Word

--- src/com/interviewPrograms/Interview_007_ReverseFirstWord.java
+++ src/com/interviewPrograms/Interview_007_ReverseFirstWord.java
@@ -2,23 +2,34 @@ package com.interviewPrograms;
 
 public class Interview_007_ReverseFirstWord 
 {
-	// Input  ->  good morning
-	// output ->  doog morning
+	// Input  ->  Java programming language
+	// output ->  avaJ programming language
 
 	public static void main(String[] args) 
 	{
-		String input = "good morning";
+        String input = "Java programming language";
 
-		// Split the input into words
-		String[] words = input.split(" ");
+        // Step 1: Split the string by space
+        String[] words = input.split(" ");
 
-		// Reverse the first word
-		String firstWordReversed = new StringBuilder(words[0]).reverse().toString();
+        // Step 2: Reverse the first word using StringBuilder
+        StringBuilder sb = new StringBuilder(words[0]);
+        words[0] = sb.reverse().toString();
 
-		// Reconstruct the final string
-		String output = firstWordReversed + " " + words[1];
+        // Step 3: Rebuild the string
+        StringBuilder result = new StringBuilder();
+        
+        for (int i = 0; i < words.length; i++) 
+        {
+            result.append(words[i]);
+            if (i < words.length - 1) 
+            {
+                result.append(" ");
+            }
+        }
 
-		System.out.println("Output: " + output);
+        // Step 4: Print result
+        System.out.println("Reversed First Word String: " + result);
+    }
 
-	}
 }
